7b64b7c080bcdc02b8226fbe2d39d2548e641880
Courtney Kalbach <courtneykalbach@Courtneys-MacBook-Pro.local>
2023-11-26 21:48:37 -0500

    updating generateMarkdown js file to stuff to write on readme

--- index.js
+++ index.js
@@ -3,106 +3,126 @@
 console.log('Welcome to the README generator! Answer the questions to create your Professional README.')
 
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown");
-const inquirer = require("inquirer")
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+
+
+// }
 
 // TODO: Create a function to initialize app
-// WHEN I am prompted for information about my application repository
-function init() {
-    // ask the questions here 
-    // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-    let responses = {
-        title: "Turtle"
+// // WHEN I am prompted for information about my application repository
+// function init() {
+//     // ask the questions here 
+//     // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+//     let data = {
 
 
 
 
-    }
-    console.log(generateMarkdown(responses))
-}
+
+//     }
+
+//     console.log(generateMarkdown(data))
+// }
+
 inquirer
-  .prompt([
-    /* Pass your questions in here */
-    // WHEN I enter my project title
-    {
-        type: 'input',
-        name: 'titleAns',
-        message: 'What is the title of your project?',
-    },
-    // WHEN I enter a description
-    {
-        type: 'input',
-        name: 'descriptionAns',
-        message: 'Write a description for your project.',
-    },
-    // WHEN I enter installation instructions
-    {
-        type: 'input',
-        name: 'installationAns',
-        message: 'Write installation instructions.',
-    },
-    // WHEN I enter usage information
-    {
-        type: 'input',
-        name: 'usageAns',
-        message: 'Provide usage information.',
-    },
-    // WHEN I enter contribution guidelines
-    {
-        type: 'input',
-        name: 'contributionAns',
-        message: 'Provide contribution guidelines.',
-    },
-    // WHEN I enter test instructions
-    {
-        type: 'input',
-        name: 'testAns',
-        message: 'Provide test instructions.',
-    },
-    // WHEN I choose a license for my application from a list of options
-    {
-        type: 'list',
-        name: 'licenseAns',
-        message: 'Choose a license.',
-        choices: [
-            'Apache License 2.0', 
-            'BSD 3-Clause', 
-            'GNU General Public License v3.0', 
-            'MIT License'
-        ]
-    },
-    // WHEN I enter my GitHub username
-    {
-        type: 'input',
-        name: 'gitHubUsernameAns',
-        message: 'Provide your GitHub username.',
-    },
-    // WHEN I enter my email address
-    {
-        type: 'input',
-        name: 'emailAns',
-        message: 'Provide email address.',
-    },
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log(answers);
-    console.log(answers.titleAns);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+    .prompt([
+        /* Pass your questions in here */
+        // WHEN I enter my project title
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        // WHEN I enter a description
+        {
+            type: 'input',
+            name: 'descriptionAns',
+            message: 'Write a description for your project.',
+        },
+        // WHEN I enter installation instructions
+        {
+            type: 'input',
+            name: 'installationAns',
+            message: 'Write installation instructions.',
+        },
+        // WHEN I enter usage information
+        {
+            type: 'input',
+            name: 'usageAns',
+            message: 'Provide usage information.',
+        },
+        // WHEN I enter contribution guidelines
+        {
+            type: 'input',
+            name: 'contributionAns',
+            message: 'Provide contribution guidelines.',
+        },
+        // WHEN I enter test instructions
+        {
+            type: 'input',
+            name: 'testAns',
+            message: 'Provide test instructions.',
+        },
+        // WHEN I choose a license for my application from a list of options
+        {
+            type: 'list',
+            name: 'licenseAns',
+            message: 'Choose a license.',
+            choices: [
+                'Apache License 2.0',
+                'BSD 3-Clause',
+                'GNU General Public License v3.0',
+                'MIT License'
+            ]
+        },
+        // WHEN I enter my GitHub username
+        {
+            type: 'input',
+            name: 'gitHubUsernameAns',
+            message: 'Provide your GitHub username.',
+        },
+        {
+            type: 'input',
+            name: 'gitHubProfileLink',
+            message: 'Provide a link to your GitHub Profile.',
+        },
+        // WHEN I enter my email address
+        {
+            type: 'input',
+            name: 'emailAns',
+            message: 'Provide email address.',
+        },
+    ])
+    .then((data) => {
+        // Use user feedback for... whatever!!
+        // console.log(answers);
+        // console.log(answers.title);
+        const readmePageContent = generateMarkdown(data);
+
+        fs.writeFile('README.md', readmePageContent, (err) =>
+            err ? console.error(err) : console.log('Successfully created README.md')
+        );
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
+
+
+
+
 // Function call to initialize app
 init();
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
-// TODO: Create a function to generate markdown for README - outputs a string
+// TODO: Create a function to generate markdown for README - outputs README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+  ${data.descriptionAns}
+  
+  ## License Badge
+  ${data.licenseAns}
+
+  ## Table of Contents
+  1. [Installation] (#Installation)
+  2. [Usage] (#Usage%20Information)
+  3. [License] (#License)
+  4. [Contributing] (#Test%20Instructions)
+  5. [Tests] (#Test%20Instructions)
+  6. [Questions] (#Questions)
+
+  ## Installation
+  ${data.installationAns}
+
+  ## Usage Information
+  ${data.usageAns} 
+
+  ## License
+  License used for this project is ${data.licenseAns}.
+  * For more information on license types, please reference this website [https: //choosealicense.com/](https://choosealicense.com/).
+
+  ## Contributing 
+  ${data.contributionAns}
+
+  ## Test Instructions
+  ${data.testAns} 
+
+  ## Questions
+  * GitHub Username: ${data.gitHubUsernameAns}
+  * [Click here to access me GitHub Profile] (${data.gitHubProfileLink})
+  * To reach me for additional question, please email me at: ${data.emailAns}
 `;
 }
 
